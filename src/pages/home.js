3b208ecbed170df845ccdd71e9ebb441d666656f
gohyinhao <yinhao.g@gmail.com>
2020-01-31 15:25:06 +0800

    remove old homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,12 +6,6 @@ class HomePage extends React.Component {
     return (
       <div className='page'>
         <NavBar />
-        <div className='homepage__container'>
-          <h1 className='homepage__container__header'>App For Me</h1>
-          <h3 className='homepage__container__subheading'>
-            Appointment & Personal Planner made for easy booking
-          </h3>
-        </div>
       </div>
     );
   }
